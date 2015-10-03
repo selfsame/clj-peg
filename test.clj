47dfea1ad4c4b7762e0df681b85cf41d833012a8
selfsame <joseph.parker.1@gmail.com>
2015-10-02 22:16:40 -0400

    test clj

---
+++ test.clj
@@ -0,0 +1,12 @@
+#(+ %1 %2)
+
+{#js :shirt #{'y 8.7 1/3}}
+
+(def euhs%243!#$%&* true) ;heheh
+
+(defn ❤ [^clojure.core/string s #_ x] 
+  (str \a '8 \&)
+    {::foobar\s
+     hard.core/clone!
+     #'user/foo}
+  (str s " is great!"))
